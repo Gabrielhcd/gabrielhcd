f3d4f7327cdb251514e577575cc39e56824d54fd
Gabriel <ghdevilmaycry@gmail.com>
2025-05-22 20:34:55 -0600

    fixed udcLogo path

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,7 +4,7 @@ function Education() {
     return(
         <div className={`card ${style.cardEducation}`}>
             <h2>Software Engineer</h2>
-            <img className={`${style.udcImgStyle}`}src="gabrielhcd/images/udcLogo.png" alt="Universidad de Colima" />
+            <img className={`${style.udcImgStyle}`}src="/gabrielhcd/images/udcLogo.png" alt="Universidad de Colima" />
             <h4>2017-2021</h4>
         </div>
     )
